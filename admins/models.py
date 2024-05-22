903a4eabee3b47caf69d9c6c5486b34521146191
salma <salma.a.samei@gmail.com>
2024-05-22 09:44:18 +0300

    update

--- admins/models.py
+++ admins/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    b_id = models.CharField(primary_key=True, max_length=50)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    author = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='photos')
+    # availability = models.BooleanField(default=True)
