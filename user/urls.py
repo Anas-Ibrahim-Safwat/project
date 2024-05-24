9fbb2a3e8640d56b2807955511200758f55c4ccc
salma <salma.a.samei@gmail.com>
2024-05-24 10:32:03 +0300

    just finished

--- user/urls.py
+++ user/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.Home, name = 'home'),
-    path('borrow/' , views.Borrow, name = 'borrow'),
-    path('browse/' , views.Browse, name = 'browse'),
-    path('detail/<str:pk>' , views.Detail, name = 'detail'),
-    path('view/', views.View , name= 'view' ),
-    path('search/', views.Search , name= 'search' )
+    path('', views.Home, name='home'),
+    path('borrow/', views.Borrow, name='borrow'),
+    path('browse/', views.Browse, name='browse'),
+    path('detail/<str:pk>', views.Detail, name='detail'),
+    path('view/', views.View, name='view'),
+    path('search/', views.Search, name='search')
 ]
