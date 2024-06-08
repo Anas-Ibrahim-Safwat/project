00f817ef288d31ef0b9f2e56fd22792b0d0327e2
hen1133 <helananemr692@gmail.com>
2024-06-08 19:22:06 +0300

    hh

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='home'),
-    path('borrow/', views.Borrow, name='borrow'),
+    path('borrow/', views.Borrowedbooks, name='borrow'),
     path('browse/', views.Browse, name='browse'),
     path('detail/<str:pk>', views.Detail, name='detail'),
     path('view/', views.View, name='viewing'),
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from admins.models import Book
 from user.models import Borrow
+from django.contrib import messages
 # Create your views here.
 
 
@@ -16,7 +17,7 @@ def View(request):
     return render(request, 'user/view-user.html', {'data': Book.objects.all()})
 
 
-def Borrow(request):
+def Borrowedbooks(request):
     borrows = Book.objects.filter(availability="Borrowed")
     return render(request, 'user/borrow-user.html', {'borrows': borrows})
     #return render(request, 'user/borrow-user.html')
@@ -33,5 +34,13 @@ def Home(request):
 
 def Detail(request, pk):
      book = get_object_or_404(Book, b_id=pk)
+     if request.method=="POST":
+         if book.availability=="Available":
+             book.availability="Borrowed"
+             book.save()
+             messages.success(request, 'Book has been borrowed successfully')
+         else:
+             messages.warning(request, 'Book cannot be borrowed')
+             return redirect( 'detail',pk)
      return render(request, 'user/book-detail.html', {'datum': book})
     #return render(request, 'user/book-detail.html')
